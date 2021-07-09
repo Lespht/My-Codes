78bc6d47aca10397d792d25e9f71e8bcfb0d2177
Lespht <josep99a2@gmail.com>
2021-07-09 08:24:54 -0600

    new project

---
+++ site/._.XD/Matrix.bat
@@ -0,0 +1,16 @@
+@echo off
+title Matrix
+color 0a
+mode 1000
+goto Greeting
+
+:Greeting
+cls
+echo Hola amig@s!!!!
+
+cls
+goto Matrix
+
+:Matrix
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto Matrix
