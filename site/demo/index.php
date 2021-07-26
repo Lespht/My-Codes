1f42bd5abd6b2d642c59fded235bc1228c97667b
lespht <josep99a2@gmail.com>
2021-07-26 10:50:01 -0600

    php

---
+++ site/demo/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link href="css.css" rel="stylesheet" type="text/css">
+        <meta charset="utf-8">
+        <title>login</title>
+    </head>
+    <body>
+    <body>
+        <div class="login-page">
+            <div class="form">
+                <div class="login-header">
+                    <h3>LOGIN</h3>
+                    <p>Please enter your credentials to login.</p>
+                </div>
+            </div>
+            <form>
+                <input type="text" placeholder="username"/>
+                <input type="password" placeholder="password"/>
+                <button>login</button>
+                <p class="message">Not registered? <a href="#">Create an Account    </a></p>
+            </form>
+        </div>
+    </body>
+    </body>
+
+</html>
